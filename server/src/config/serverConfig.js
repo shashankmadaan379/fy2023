77056c74882c0b32c501b5bf9c677a22c63923a0
shashankmadaan379 <codebreaker379@gmail.com>
2023-05-28 01:08:28 +0530

    added dalle route

--- server/src/config/serverConfig.js
+++ server/src/config/serverConfig.js
@@ -5,4 +5,7 @@ module.exports = {
   MONGO_DB_URL: process.env.MONGO_DB_URL,
   MODE: process.env.MODE,
   OPEN_AI_KEY: process.env.OPEN_AI_KEY,
+  CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME,
+  CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+  CLOUDINARY_SECRET_KEY: process.env.CLOUDINARY_SECRET_KEY,
 };
--- server/src/routes/v1/post.js
+++ server/src/routes/v1/post.js
@@ -1,3 +1,19 @@
 const express = require("express");
+const { cloudinary } = require("cloudinary");
+const {
+  CLOUDINARY_API_KEY,
+  CLOUDINARY_SECRET_KEY,
+  CLOUDINARY_CLOUD_NAME,
+} = require("../../config/serverConfig");
 const router = express.Router();
+
+cloudinary.config({
+  cloud_name: CLOUDINARY_CLOUD_NAME,
+  api_key: CLOUDINARY_API_KEY,
+  api_secret: CLOUDINARY_SECRET_KEY,
+});
+
+router.get("/", async (req, res) => {});
+router.post("/", async (req, res) => {});
+
 module.exports = router;
