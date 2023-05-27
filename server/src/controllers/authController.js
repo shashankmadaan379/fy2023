acbfb1d6a108269bf17c508fe87ee92abdf5cc5d
shashankmadaan379 <codebreaker379@gmail.com>
2023-05-27 16:20:29 +0530

    added login controller

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -31,3 +31,43 @@ exports.registerUser = async (req, res) => {
     });
   }
 };
+
+exports.loginController = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "Please enter all required fields",
+      });
+    }
+    const user = await User.findOne({ email }).select("+password");
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "Invalid Email or Password",
+      });
+    }
+
+    const isMatchedPassword = await user.matchPassword(password);
+    if (!isMatchedPassword) {
+      return res.status(404).json({
+        success: false,
+        message: "Invalid Email or Password",
+      });
+    }
+
+    return res.status(201).json({
+      success: true,
+      message: "Login successfully",
+      data: user,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: "Something went wrong in the login process",
+      err: error,
+    });
+  }
+};
